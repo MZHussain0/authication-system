d3ea325e7d6ae47e8651035ef6e0b0c3b49b176f
mzhussain0 <mzhussain092@gmail.com>
2023-04-14 03:56:56 +0530

    client: integrate formik

--- client/src/components/Username.jsx
+++ client/src/components/Username.jsx
@@ -2,10 +2,25 @@
 import { Link } from "react-router-dom";
 import avatar from "../assets/profile.png";
 import styles from "../styles/Username.module.css";
+import { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
+import { usernameValidate } from "../helper/validate";
 
 const Username = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+    },
+    validate: usernameValidate,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+  });
   return (
     <div className="container mx-auto">
+      <Toaster position="top-center" reverseOrder={false} />
       <div className="flex justify-center items-center h-screen">
         <div className={styles.glass}>
           <div className="title flex flex-col items-center">
@@ -15,21 +30,20 @@ const Username = () => {
             </span>
           </div>
 
-          <form action="" className="py-1">
+          <form action="" className="py-1" onSubmit={formik.handleSubmit}>
             <div className="profile flex justify-center py-4">
               <img src={avatar} alt="avatar" className={styles.profile_img} />
             </div>
 
-            <div className="textbox flex flex-col items-center gap-4">
+            <div className="textbox flex flex-col items-center gap-6">
               <input
+                {...formik.getFieldProps("username")}
                 className={styles.textbox}
                 type="text"
-                name=""
-                id=""
                 placeholder="username"
               />
-              <button type="submit" className={styles.btn}>
-                Let's go
+              <button className={styles.btn} type="submit">
+                Let's Go
               </button>
             </div>
 
---
+++ client/src/helper/validate.js
@@ -0,0 +1,18 @@
+﻿import toast from "react-hot-toast";
+
+// validate login page username
+export async function usernameValidate(values) {
+  const errors = usernameVerify({}, values);
+  return errors;
+}
+
+// Validate username
+function usernameVerify(error = {}, values) {
+  if (!values.username) {
+    error.username = toast.error("Username must be provided..!");
+  } else if (values.username.includes(" ")) {
+    error.username = toast.error("Invalid username..!");
+  }
+
+  return error;
+}
