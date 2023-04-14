cb6df7b87c2424f59643ef7acf8fdedc8cd31d5a
mzhussain0 <mzhussain092@gmail.com>
2023-04-14 15:54:37 +0530

    client: register and profile UI

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -20,6 +20,18 @@ export async function resetPasswordValidation(values) {
   return errors;
 }
 
+export async function registerValidation(values) {
+  const errors = usernameVerify({}, values);
+  passwordVerify(errors, values);
+  emailVerify(errors, values);
+  return errors;
+}
+
+export async function profileValidation(values) {
+  const errors = emailVerify({}, values);
+  return errors;
+}
+
 // ----------------------------------------------------------- //
 // Validate password
 function passwordVerify(error = {}, values) {
@@ -45,6 +57,19 @@ function usernameVerify(error = {}, values) {
     error.username = toast.error("Username must be provided..!");
   } else if (values.username.includes(" ")) {
     error.username = toast.error("Invalid username..!");
+  }
+
+  return error;
+}
+
+// Validate email
+function emailVerify(error = {}, values) {
+  if (!values.email) {
+    error.email = toast.error("Email must be provided..!");
+  } else if (values.email.includes(" ")) {
+    error.email = toast.error("Invalid email..!");
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    error.email = toast.error("Invalid email address...!");
   }
 
   return error;
