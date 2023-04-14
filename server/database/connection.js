688efbb6962817cb5351b13696b5d40b2671866f
mzhussain0 <mzhussain092@gmail.com>
2023-04-14 23:18:55 +0530

    server setup with mongodb

---
+++ server/database/connection.js
@@ -0,0 +1,15 @@
+﻿import mongoose from "mongoose";
+
+const connect = async () => {
+  try {
+    console.log(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default connect;
---
+++ server/server.js
@@ -0,0 +1,28 @@
+﻿import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import connect from "./database/connection.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+connect();
+const app = express();
+const PORT = 8000;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("tiny"));
+app.disable("x-powered-by");
+
+// HTTP Request //
+app.get("/", (req, res) => {
+  res.status(201).json("Home get request");
+});
+
+// start server only when we have valid connection //
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+});
